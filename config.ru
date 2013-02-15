2253a4927bc3eefc3bf9f0280ebeb02f5420f540
Adrian Kuciel <djmicros@gmail.com>
2013-02-15 18:05:04 +0100

    first commit

---
+++ config.ru
@@ -0,0 +1,4 @@
+# This file is used by Rack-based servers to start the application.
+
+require ::File.expand_path('../config/environment',  __FILE__)
+run Railsy1::Application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsy1::Application.config.secret_token = '3fe2aec0a7e3d2387f53ece618e8acfc3fde227eb6a2bf4b5fd0de6659d4be89b3280c45f60cffdea04299c014563fd5cc1ef6c9f43d1b43874136a50782f021'
